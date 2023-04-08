20ceefed4e4cd1e7fe69bd45ad9349f9fd09c2ab
Adam Mehdi <adammehdi98765@gmail.com>
2023-04-08 16:01:09 -0400

    Basis for obstacle class

---
+++ car-details.py
@@ -0,0 +1,122 @@
+from pygame import *
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Obstacle:
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+
+
+class ParkedCar(Obstacle):
+    def __init__(self, xpos, ypos, width, length):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.width = width
+        self.length = length
+
+        super().__init__(xpos, ypos)
+
+    def isColliding(self, car):
+        """Checks for collision between two rotated rectangles"""
+
+        #TODO: Properly orient car with rotation + complex nums
+
+        newXpos = self.xpos
+        newYpos = self.ypos
+
+        distx = abs(self.xpos - car.xpos)
+        disty = abs(self.ypos - car.ypos)
+
+        return True
+
+    def update(self, screen):
+        draw.rect(screen, (200, 200, 200), (self.xpos - self.width//2, self.ypos-self.length//2, self.width, self.length), 0)
+
+
+class LightPost(Obstacle):
+    def __init__(self, xpos, ypos, radius):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.radius = radius
+        super().__init__(xpos, ypos)
+
+    def isColliding(self, car):
+        """Checks for collision between circle and rotated rectangle"""
+
+        #TODO: Properly orient car with rotation + complex nums
+
+        newXpos = self.xpos
+        newYpos = self.ypos
+
+        distx = abs(newXpos - car.xpos)
+        disty = abs(newYpos - car.ypos)
+
+        if distx > (car.width/2 + self.radius):
+            return False
+        if disty > (car.length/2 + self.radius):
+            return False
+
+        if distx <= car.width/2:
+            return True
+        if disty <= car.length:
+            return True
+
+        cornerDist = (distx - car.width/2)**2 + (disty - car.length/2)**2
+
+        return cornerDist <= self.radius**2
+
+
+    def update(self, screen):
+        draw.circle(screen, (200, 200, 200), (self.xpos. self.ypos), self.radius, 0)
+
+
+class Curb(Obstacle):
+    def __init__(self, xpos, ypos, width, length):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.width = width
+        self.length = length
+
+    def isColliding(self, car):
+
+        #Properly orient car with rotation + complex nums
+        newXpos = self.xpos
+        newYpos = self.ypos
+
+        distx = abs(self.xpos - car.xpos)
+        disty = abs(self.ypos - car.ypos)
+
+        return True
+
+    def update(self, screen):
+        draw.rect(screen, (200, 200, 200), (self.xpos - self.width//2, self.ypos-self.length//2, self.width, self.length), 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
